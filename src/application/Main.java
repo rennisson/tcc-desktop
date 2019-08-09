531b64651c1183379dd4d019003427b00dcc9dd4
rennisson <rennisson.alves@gmail.com>
2019-08-08 22:01:32 -0300

    Classe da entidade Cliente

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import model.entities.Cliente;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
@@ -39,7 +40,7 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Cliente obj = new Cliente(1, "rennisson.alves@gmail.com", "rennisson", "11-981936306");
+		System.out.println(obj);
 	}
 }
---
+++ src/model/entities/Cliente.java
@@ -0,0 +1,85 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Cliente implements Serializable {	
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer codigo;
+	private String email;
+	private String nome;
+	private String telefone;
+	
+	public Cliente() {
+	}
+	
+	public Cliente(Integer codigo, String email, String nome, String telefone) {
+		this.codigo = codigo;
+		this.email = email;
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [codigo=" + codigo + ", email=" + email + ", nome=" + nome + ", telefone=" + telefone + "]";
+	}	
+}
