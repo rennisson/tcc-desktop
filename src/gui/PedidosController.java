07300e6f9517d6f8d719ca6669e7ff0a523eccb9
rennisson <rennisson.alves@gmail.com>
2019-08-17 21:31:39 -0300

    PedidoForm (dialog) design

--- src/gui/PedidosController.java
+++ src/gui/PedidosController.java
@@ -1,22 +1,31 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.entities.Pedido;
 import model.services.PedidoService;
 
@@ -91,7 +100,7 @@ public class PedidosController implements Initializable {
 					}
 					setGraphic(button);
 					button.setOnAction(
-							event -> System.out.println(""));
+							event -> createDialogForm("/gui/PedidoForm.fxml", Utils.currentStage(event)));
 				}
 		});
 		
@@ -107,7 +116,7 @@ public class PedidosController implements Initializable {
 					}
 					setGraphic(button);
 					button.setOnAction(
-							event -> System.out.println(""));
+							event -> createDialogForm("/gui/PedidoForm.fxml", Utils.currentStage(event)));
 				}
 		});
 	}
@@ -120,5 +129,23 @@ public class PedidosController implements Initializable {
 		obsList = FXCollections.observableArrayList(list);
 		tableViewPedidos.setItems(obsList);
 		initEditButtons();
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("DETALHES DO PEDIDO");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
